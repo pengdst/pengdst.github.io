76c92ff757fbd58f87ad94a62c24047b4cc174fb
pengdst <pengkuh.dst@gmail.com>
2023-04-26 16:38:44 +0700

    renew portfolio with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        primary: '#6b7280',
+        primaryBackground: '#f0f9ff',
+        primaryText: '#0f172a',
+        secondary: '',
+        secondaryBackground: '#fff',
+        secondaryText: '#64748b',
+        dark: '#0f172a',
+        darkLight: '#64748b'
+      }
+    },
   },
   plugins: [],
 }
