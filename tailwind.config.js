c13f077fda615d2ff521fdcc98b67583f9b09d00
pengdst <pengkuh.dst@gmail.com>
2023-04-27 07:14:29 +0700

    adjust astro and tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     container: {
       center: true
